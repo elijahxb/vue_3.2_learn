450e7c1ffda3855a63259103496855d66a6b61dd
elijahxb <26068420+elijahxb@users.noreply.github.com>
2023-02-06 00:20:04 +0800

    fix: commitlint配置错误修复

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,22 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: [
-        2,
-        'always',
-        [
-            'feat',
-            'fix',
-            'docs',
-            'style',
-            'refactor',
-            'perf',
-            'test',
-            'chore',
-            'revert',
-            'build'
-        ]
-    ],
-    'subject-case': [0]
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'chore',
+                'revert',
+                'build'
+            ]
+        ],
+        'subject-case': [0]
+    }
 }
