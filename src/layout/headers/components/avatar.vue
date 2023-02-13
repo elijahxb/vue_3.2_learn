163b0cffd6e6571f044fef3202b9c744942fb60a
elijahxb <26068420+elijahxb@users.noreply.github.com>
2023-02-14 00:25:19 +0800

    feat: 头像与登出

---
+++ src/layout/headers/components/avatar.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-dropdown>
+    <el-avatar shape="square" :size="40" :fit='fill' :src="avatarUrl" />
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item @click='logout'>退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+const avatarUrl = ref(
+  'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+)
+
+const logout = () => {
+  store.dispatch('app/logout')
+}
+
+</script>
+
+<style lang='scss' scoped>
+::v-deep .el-dropdown-menu__item {
+  white-space: nowrap;
+}
+</style>
--- src/layout/headers/index.vue
+++ src/layout/headers/index.vue
@@ -2,11 +2,15 @@
   <div class='navbar' >
     <Fold id='siderTypeId' @click='toggleClick'></Fold>
     <Breadcrumb />
+    <div class='navbar-right'>
+      <Avatar />
+    </div>
   </div>
 </template>
 
 <script setup>
 import Breadcrumb from './components/breadcrumb'
+import Avatar from './components/avatar'
 import { Fold } from '@element-plus/icons-vue'
 import { useStore } from 'vuex'
 import { computed } from 'vue'
@@ -24,6 +28,20 @@ const icon = computed(() => {
 #siderTypeId {
   height: 25px;
   cursor: pointer;
+}
+.user-avatar {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ::v-deep .navbar-item {
+    display: inline-block;
+    margin-left: 18px;
+    font-size: 22px;
+    color: #5a5e66;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
 }
 .navbar {
   width: 100%;
