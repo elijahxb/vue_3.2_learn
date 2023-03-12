d0116dd33bef03a111672b519966d9d587585da3
elijahxb <26068420+elijahxb@users.noreply.github.com>
2023-03-13 07:06:51 +0800

    feat: 登陆加密

--- src/api/login.js
+++ src/api/login.js
@@ -1,9 +1,17 @@
 import request from './request'
+import { sha256 } from 'js-sha256'
+import { Base64 } from 'js-base64'
+
+const ts = timestamp()
+
+function timestamp() {
+  return Date.parse(new Date().toString())
+}
 
 export const login = (data) => {
   return request({
-    url: '/login',
+    url: '/login?ts=' + ts,
     method: 'POST',
-    data
+    data: { username: data.username, password: sha256(Base64.encode(data.password + ts)) }
   })
 }
